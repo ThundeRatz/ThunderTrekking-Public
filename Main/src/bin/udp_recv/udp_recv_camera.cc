3fa9b86c747a4cfeedec392d218ac458965c86aa
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2015-09-07 00:33:43 -0300

    Licence added, hc-sr04 related files, udp_packets.h and main_v0.1.cc removed

--- Main/src/bin/udp_recv/udp_recv_camera.cc
+++ Main/src/bin/udp_recv/udp_recv_camera.cc
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 ThundeRatz
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 
@@ -8,23 +32,23 @@
 int main() {
 	int udp_socket;
 	camera_data data;
-	
+
 	if ((udp_socket = udp_receiver_init(UDP_CAMERA, sizeof(camera_data))) == -1) {
 		perror("udp_sender_init");
 		return -1;
 	}
-	
+
 	for (;;) {
 		switch (udp_receiver_recv(udp_socket, &data, sizeof(data))) {
 			case sizeof(data):
 			printf("%f %f %f %f\n", data.top_left.x, data.top_left.y,
 				data.bottom_right.x, data.bottom_right.y);
 			break;
-			
+
 			case -1:
 			perror("recvfrom");
 			return -1;
-			
+
 			default:
 			printf("Unexpected message size\n");
 			break;
--- Main/src/bin/udp_recv/udp_recv_hmc5883l.cc
+++ Main/src/bin/udp_recv/udp_recv_hmc5883l.cc
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 ThundeRatz
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <arpa/inet.h>
@@ -11,13 +35,13 @@ int main() {
 	struct sockaddr_in remote;
 	char ip[41];
 	int16_t data[3];
-	
+
 	udp_socket = udp_receiver_init(UDP_HMC5883L, sizeof(data));
 	if (udp_socket == -1) {
 		perror("udp_sender_init");
 		return -1;
 	}
-	
+
 	for (;;) {
 		switch (udp_receiver_recv(udp_socket, &data, sizeof(data))) {
 			case sizeof(data):
@@ -27,11 +51,11 @@ int main() {
 			} else
 				printf("%s: %hd %hd %hd %lf\n", ip, data[0], data[1], data[2], compass_orientation(data[0], data[2]));
 			break;
-			
+
 			case -1:
 			perror("udp_receiver_recv");
 			return -1;
-			
+
 			default:
 			printf("Unexpected message size\n");
 			break;

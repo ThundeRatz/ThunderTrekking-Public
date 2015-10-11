522f221cf48ab40fe76a6f83d1bbc3bf52c199ec
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2015-10-10 21:54:26 -0300

    Leds moved to LedsI2C
    Leds linux susbsystem driver will be added

--- Main/src/lib/LedsI2C.cc
+++ Main/src/lib/LedsI2C.cc
@@ -1,7 +1,7 @@
-#include "Leds.hh"
+#include "LedsI2C.hh"
 namespace Trekking {
 
-	Leds::Leds() {
+	LedsI2C::LedsI2C() {
 		i2c.acquire(0x24);
 		i2c[MODE] = MANUAL;
 		i2c[CORR] = 0;
@@ -10,71 +10,68 @@ namespace Trekking {
 		i2c.release();
 	}
 
-	Leds::~Leds(){
-	}
 
-
-	void Leds::setMode(unsigned value) {
+	void LedsI2C::setMode(unsigned value) {
 		i2c.acquire(0x24);
 		i2c[MODE] = value;
 		i2c.release();
 	}
 
-	void Leds::setTimestep(unsigned value) {
+	void LedsI2C::setTimestep(unsigned value) {
 		i2c.acquire(0x24);
 		i2c[TIMESTEP] = value;
 		i2c.release();
 	}
 
 
-	void Leds::red(unsigned value) {
+	void LedsI2C::red(unsigned value) {
 		i2c.acquire(0x24);
 		i2c[CORR] = value;
 		i2c.release();
 	}
 
-	void Leds::green(unsigned value) {
+	void LedsI2C::green(unsigned value) {
 		i2c.acquire(0x24);
 		i2c[CORG] = value;
 		i2c.release();
 	}
 
-	void Leds::blue(unsigned value) {
+	void LedsI2C::blue(unsigned value) {
 		i2c.acquire(0x24);
 		i2c[CORB] = value;
 		i2c.release();
 	}
 
-	void Leds::white(unsigned value) {
+	void LedsI2C::white(unsigned value) {
 		i2c.acquire(0x24);
-		i2c[CORR] = 255;
-		i2c[CORG] = 255;
-		i2c[CORB] = 255;
+		i2c[CORR] = value;
+		i2c[CORG] = value;
+		i2c[CORB] = value;
 		i2c.release();
 	}
 
-	void Leds::apagaLeds() {
+	void LedsI2C::apaga() {
 		i2c.acquire(0x24);
 		i2c[CORR] = 0;
 		i2c[CORG] = 0;
 		i2c[CORB] = 0;
 		i2c.release();
 	}
-	void Leds::blink(unsigned timestep) {
+	void LedsI2C::blink(unsigned timestep) {
 		i2c.acquire(0x24);
 		setMode(BLINK);
 		setTimestep(timestep);
 		i2c.release();
 	}
 
-	void Leds::pulse(unsigned timestep) {
+	void LedsI2C::pulse(unsigned timestep) {
 		i2c.acquire(0x24);
 		setMode(PULSE);
 		setTimestep(timestep);
 		i2c.release();
 	}
 
-	void Leds::pulse3(unsigned timestep) {
+	void LedsI2C::pulse3(unsigned timestep) {
 		i2c.acquire(0x24);
 		setMode(PULSE3);
 		setTimestep(timestep);

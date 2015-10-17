291fa7e1c56df9ce7aef5cddc71ac88bc24a8370
Daniel Nery <danielnso@outlook.com>
2015-10-17 10:32:49 +0000

    Mais testador geral

--- Main/src/bin/utils/TestaTudo.cc
+++ Main/src/bin/utils/TestaTudo.cc
@@ -10,6 +10,7 @@
 #include "BNO055.hh"
 #include "Bumper.hh"
 #include "sleep.hh"
+#include "Leds.hh"
 #include "GPS.hh"
 
 using namespace Trekking;
@@ -19,17 +20,13 @@ static WINDOW* pixy_s;
 static WINDOW* bno_s;
 static WINDOW* bumper_s;
 
-static pixy_block_t block;
-static GPSMonitor gps(GPS(0., 0.));
-static BNO055 bno055;
-static Bumper bumper;
-
-void GPSScreen() {
-	gps.update();
-
+void GPSScreen(GPSMonitor& gps) {
 	wclear(gps_s);
+	if(!gps.update())
+		mvwprintw(gps_s, LINES/2 - 6 - 5, 1, "blocking_update error");
+
 	box(gps_s, 0 , 0);
-	mvwprintw(gps_s, 1, 3, "GPS");
+	mvwprintw(gps_s, 1, (COLS/2 - 6)/2 - 1, "GPS");
 
 	mvwprintw(gps_s, 3, 1, "Latitude: %hu", gps.latitude);
 	mvwprintw(gps_s, 4, 1, "Longitude: %hu", gps.longitude);
@@ -37,12 +34,12 @@ void GPSScreen() {
 	wrefresh(gps_s);
 }
 
-void PixyScreen() {
+void PixyScreen(pixy_block_t& block) {
 	pixy_cam_get(&block);
 
 	wclear(pixy_s);
 	box(pixy_s, 0 , 0);
-	mvwprintw(pixy_s, 1, 3, "Pixy Cam");
+	mvwprintw(pixy_s, 1, (COLS/2 - 6)/2 - 4, "Pixy Cam");
 
 	mvwprintw(pixy_s, 3, 1, "Object Type: %hu", block.type);
 	mvwprintw(pixy_s, 4, 1, "Object Signature: %hu", block.signature);
@@ -55,7 +52,7 @@ void PixyScreen() {
 	wrefresh(pixy_s);
 }
 
-void BNOScreen() {
+void BNOScreen(BNO055& bno055) {
 	Eigen::Vector2d acceleration;
 	Eigen::Rotation2D<double> heading;
 
@@ -64,31 +61,48 @@ void BNOScreen() {
 
 	wclear(bno_s);
 	box(bno_s, 0 , 0);
-	mvwprintw(bno_s, 1, 3, "Sensor de 9 eixos");
+	mvwprintw(bno_s, 1, (COLS/2 - 6)/2 - 3, "9 eixos");
 
-	mvwprintw(bno_s, 5, 1, "Acceleration: %f %f", acceleration[0], acceleration[1]);
-	mvwprintw(bno_s, 9, 1, "Heading: %f", heading.angle());
+	mvwprintw(bno_s, 5, 1, "Acceleration: %.0f %.0f", acceleration[0], acceleration[1]);
+	mvwprintw(bno_s, 6, 1, "Heading: %f", heading.angle());
 
 	wrefresh(bno_s);
 }
 
-void BumperScreen() {
+void BumperScreen(Bumper& bumper) {
 	wclear(bumper_s);
 	box(bumper_s, 0, 0);
-	mvwprintw(bumper_s, 1, 3, "Bumper");
+	mvwprintw(bumper_s, 1, (COLS/2 - 6)/2 - 3, "Bumper");
 
 	if (bumper.pressed())
-		mvwprintw(bno_s, 5, 1, "Pressed");
+		mvwprintw(bumper_s, 5, 1, "Pressed");
 	else
-		mvwprintw(bno_s, 5, 1, "Released");
+		mvwprintw(bumper_s, 5, 1, "Released");
 
 	wrefresh(bumper_s);
 }
 
 int main() {
+	pixy_block_t block;
+	GPSMonitor gps(GPS(0., 0.));
+	BNO055 bno055;
+	Bumper bumper;
+	char c;
+
+	Leds ledr("LedRed");
+	Leds ledg("LedGreen");
+	Leds ledb("LedBlue");
+
+	bool ledrOn = false;
+	bool ledgOn = false;
+	bool ledbOn = false;
+
 	pixy_cam_init();
 
 	initscr();
+	raw();
+	noecho();
+	keypad(stdscr, TRUE);
 
 	gps_s = newwin(LINES/2 - 6, COLS/2 - 6, 2, 2);
 	box(gps_s, 0 , 0);
@@ -96,23 +110,35 @@ int main() {
 	pixy_s = newwin(LINES/2 - 6, COLS/2 - 6, 2, COLS/2 + 1);
 	box(pixy_s, 0 , 0);
 
-	bno_s = newwin(LINES/2 - 6, COLS/2 - 6, LINES/2 + 1, 2);
+	bno_s = newwin(LINES/2 - 6, COLS/2 - 6, LINES/2, 2);
 	box(bno_s, 0 , 0);
 
-	bumper_s = newwin(LINES/2 - 6, COLS/2 - 6, LINES/2 + 1, COLS/2 + 1);
+	bumper_s = newwin(LINES/2 - 6, COLS/2 - 6, LINES/2, COLS/2 + 1);
 	box(bumper_s, 0 , 0);
 
+	mvprintw(LINES - 1, 3, "LEDs: r - toggle red, g - toggle green, b - toggle blue | q - quit");
+
+	refresh();
 	wrefresh(gps_s);
 	wrefresh(pixy_s);
 	wrefresh(bno_s);
 	wrefresh(bumper_s);
 
+	timeout(90);
 	for (;;) {
-		GPSScreen();
-		PixyScreen();
-		BNOScreen();
-		BumperScreen();
-		sleep_ms(200);
+		c = getch();
+		switch (c) {
+			case 'r': ledr = (ledrOn ? 0 : 1); ledrOn = !ledrOn; break;
+			case 'g': ledg = (ledgOn ? 0 : 1); ledgOn = !ledgOn; break;
+			case 'b': ledb = (ledbOn ? 0 : 1); ledbOn = !ledbOn; break;
+			case 'q': endwin(); return 0; break;
+			default: break;
+		}
+		GPSScreen(gps);
+		PixyScreen(block);
+		BNOScreen(bno055);
+		BumperScreen(bumper);
+		sleep_ms(10);
 	}
 
 	endwin();

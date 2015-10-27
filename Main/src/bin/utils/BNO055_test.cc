2507680c3a9fc86ead6472a42a0723daced5903a
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2015-10-27 19:28:08 -0200

    criação do arquivo.

--- Main/src/bin/utils/BNO055_test.cc
+++ Main/src/bin/utils/BNO055_test.cc
@@ -1,19 +1,34 @@
 #include <eigen3/Eigen/Dense>
 #include <iostream>
-
+#include <cstdlib>
 #include "BNO055.hh"
 #include "sleep.hh"
 
 int main() {
+	FILE *file;
 	Trekking::BNO055 bno055;
 	Eigen::Vector2d acceleration;
 	Eigen::Rotation2D<double> heading;
+	struct bno055_sic_matrix_t *p;
+	u8 *v_mag_calib_u8;
+	u8 *v_accel_calib_u8;
+	u8 *v_gyro_calib_u8;
+	u8 *v_sys_calib_u8;
+	
+	file = fopen("Calibration.txt", "w");
+	bno055_get_mag_calib_stat(v_mag_calib_u8);
+	bno055_get_accel_calib_stat(v_accel_calib_u8);
+	bno055_get_gyro_calib_stat(v_gyro_calib_u8);
+	bno055_get_sys_calib_stat(v_sys_calib_u8);
+	fprintf(file, "%d %d %d %d", *v_mag_calib_u8, *v_accel_calib_u8, *v_gyro_calib_u8, *v_sys_calib_u8);
+	fclose(file);
+	/*
 	for (;;) {
 		bno055.linear_acceleration(acceleration);
 		bno055.heading(heading);
 		std::cout << "Accel: " << acceleration[0] << ' ' << acceleration[1] << '\n';
 		std::cout << "Heading: " << heading.angle() << '\n';
 		Trekking::sleep_ms(100);
-	}
+	}*/
 	return 0;
 }

833d56f204da348cb12cdc95f39680d72b9f7ab0
Daniel Nery <danielnso@outlook.com>
2015-10-28 05:49:33 -0200

    Tests

--- Main/src/bin/utils/GPSTest.cc
+++ Main/src/bin/utils/GPSTest.cc
@@ -4,6 +4,9 @@
 
 #include "GPS.hh"
 #include "sleep.hh"
+#include "BNO055.hh"
+
+#include "compass.h"
 
 using namespace std;
 using namespace Trekking;
@@ -12,18 +15,24 @@ int main() {
 	cout << fixed << setprecision(10);
 	GPS origem(-0.41109563, -0.8155735890);
 	GPSMonitor gps(origem);
+	BNO055 bno;
+	static Eigen::Rotation2D<double> heading;
+
 	for (;;) {
 		if(!gps.blocking_update()) {
 			//cout << "Blocking update\n";
 			sleep_ms(100);
 			continue;
 		}
+		bno.heading(heading);
 		cout << "Latitude:  " << gps.latitude
 			<< "\nLongitude: " << gps.longitude
 			<< "\nX: " << gps.point[0]
 			<< "\nY: " << gps.point[1] << endl;
 		cout << "\nDistancia: " << gps.distance_to(origem) << " km" << endl
-		     <<   "           " << sqrt(gps.point[0]*gps.point[0] + gps.point[1]*gps.point[1]) << " km" << endl << endl << endl;
+		     <<   "           " << sqrt(gps.point[0]*gps.point[0] + gps.point[1]*gps.point[1]) << " km" << endl;
+		cout << "Azimuth: " << gps.azimuth_to(origem) << endl;
+		cout << "Angle Diff: " << compass_diff(gps.azimuth_to(origem), heading.angle()) << endl << endl << endl;
 		sleep_ms(50);
 	}
 }
